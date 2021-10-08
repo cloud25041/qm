41b9a412e736d57f6097048b3da9b32d1de17a14
Eric Tze Yong Oon <eric.oon@ncs.com.sg>
2021-10-08 16:08:50 +0800

    added persistant storage into ui proj

---
+++ UI/LocalStorage/LocalStorageService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace UI.LocalStorage
+{
+    public class LocalStorageService : ILocalStorageService
+    {
+        private IJSRuntime _jsRuntime;
+        public LocalStorageService(IJSRuntime jSRuntime)
+        {
+            _jsRuntime = jSRuntime;
+        }
+
+        public Task<T> GetItem<T>(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveItem(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetItem<T>(string username, T userDetails)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
