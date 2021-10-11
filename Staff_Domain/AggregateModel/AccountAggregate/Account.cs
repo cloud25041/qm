1def391e88cfebd092a084fef2c39be8e13a812f
Eric Tze Yong Oon <eric.oon@ncs.com.sg>
2021-10-11 19:20:09 +0800

    edited local storage to make it more simple

--- Staff_Domain/AggregateModel/AccountAggregate/Account.cs
+++ Staff_Domain/AggregateModel/AccountAggregate/Account.cs
@@ -7,5 +7,12 @@ namespace Staff_Domain.AggregateModel.AccountAggregate
 {
     public class Account
     {
+        public Guid AccountId { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+        public string Mobile { get; private set; }
+        public Agency Agency { get; private set; }
+        public Schedule Schedule { get; private set; }
     }
 }
