de0fb2924741b6d60ff12392862d5294577859bf
Zhuojing Wang <zhuojing.wang@ncs.com.sg>
2021-10-14 12:22:37 +0800

    created appointmentdetails model and edited appointmentcontroller, changed connection string in appointmentviewallappointment page

--- AR_API/Controllers/AppointmentController.cs
+++ AR_API/Controllers/AppointmentController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using MediatR;
 using AR_Application.Commands;
 using AR_Application.Queries;
+using AR_API.Models;
 
 namespace AR_API.Controllers
 {
@@ -28,6 +29,39 @@ namespace AR_API.Controllers
             List<AppointmentViewModel> listOfAppointment = await _appointmentQueries.GetAllAppointments();
             return listOfAppointment;
         }
+
+        [Route("api/UserQueue/GetAppointmentDetails")]
+        [HttpPost]
+        public IList<AppointmentDetails> GetAppointmentDetails([FromBody] string accountId)
+        {
+            IList<AppointmentDetails> appointmentDetails = new List<AppointmentDetails>();
+
+            // dummy data
+            string[] agencyArray = new string[3] { "HDB", "CPF", "MSF" };
+            string[] serviceArray = new string[3] { "Service1", "Service2", "Service3" };
+            string[] locationArray = new string[3] { "Location1", "Lovation2", "Location3" };
+            // DateTime[] appointmentTimeArray = new DateTime[3] {"20210928 02:50:30.840252","20210928 02:50:30.840252", "20210928 02:50:30.840252" }
+
+
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                AppointmentDetails apptDetails = new AppointmentDetails();
+                apptDetails.AppointmentName = i.ToString();
+                apptDetails.AppointmentTime = DateTime.Now;
+                apptDetails.Location = locationArray[i];
+                apptDetails.Service = serviceArray[i];
+                apptDetails.Agency = agencyArray[i];
+
+                appointmentDetails.Add(apptDetails);
+            }
+            // appointmentDetails.AppointmentName = "12345678";
+            return appointmentDetails;
+
+        }
+
+
 
         [Route("api/account/getallappointmentsbyaccountid")]
         [HttpGet]
---
+++ AR_API/Models/AppointmentDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AR_API.Models
+{
+    public class AppointmentDetails
+    {
+
+        public Guid TransactionId { get; set; }
+        public string AppointmentName { get; set; }
+
+        public DateTime AppointmentTime { get; set; }
+        public string Agency { get; set; }
+        public string Service { get; set; }
+        public string Location { get; set; }
+
+        public int AppointmentDetailId { get; set; }
+
+    }
+}
