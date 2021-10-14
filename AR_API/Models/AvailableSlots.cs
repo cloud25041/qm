66bec9f85d0118b4c643a47586c919036f1c1570
Ren Jie Lou <renjie.lou@ncs.com.sg>
2021-10-14 12:17:28 +0800

    Added new models in AR_API

---
+++ AR_API/Models/AvailableSlots.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AR_API.Models
+{
+    public class AvailableSlots
+    {
+
+        public DateTime Date { get; set; }
+
+        public string Time { get; set; }
+
+        public int SlotId { get; set; }
+    }
+}
---
+++ AR_API/Models/UserAgencyInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AR_API.Models
+{
+    public class UserAgencyInput
+    {
+        public int? AgencyId { get; set; }
+
+        public int? AppointmentTypeId { get; set; }
+
+
+
+    }
+}
