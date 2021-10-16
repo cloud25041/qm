27e287cb1bfb17c1bd23620c1ec7e0097268db12
Ren Jie Lou <renjie.lou@ncs.com.sg>
2021-10-16 18:26:26 +0800

    Added new ViewModel

---
+++ Staff_Application/Queries/AgencyViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Staff_Application.Queries
+{
+    public record AgencyViewModel
+    {
+        public string AgencyName { get; init; }
+        public int AgencyId { get; init; }
+        public string AgencyPin { get; init; }
+        public string Location { get; init; }
+        public int PhysicalConcurrentUser { get; init; }
+        public int VirtualConcurrentUser { get; init; }
+    }
+}
