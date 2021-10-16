ace4c67c3a59f150371d45bf0b4b2bf48a2b6888
Ren Jie Lou <renjie.lou@ncs.com.sg>
2021-10-16 18:30:20 +0800

    Added new ViewModels in AR_Application

---
+++ AR_Application/Model/AvailableSlotsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AR_Application.Model
+{
+    public class AvailableSlotsViewModel
+    {
+        public DateTime? Date { get; init; }
+
+        public string? Time { get; init; }
+
+        public int SlotId { get; init; }
+    }
+}
