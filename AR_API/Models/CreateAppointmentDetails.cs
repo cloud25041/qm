38f678c47942a7fdd66e544ce529d100a5a8b32b
Ren Jie Lou <renjie.lou@ncs.com.sg>
2021-10-23 15:00:40 +0800

    Added new model for Creating Appointments

---
+++ AR_API/Models/CreateAppointmentDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AR_API.Models
+{
+    public class CreateAppointmentDetails
+    {
+        public int AgencyId { get; set; }
+        public int AppointmentType { get; set; }
+        public int AppointmentState { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public int AppointmentSlotId { get; set; }
+        public Guid UserAccountId { get; set; }
+    }
+}
+
