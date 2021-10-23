5621dca99cc6575fb51041686b29eabd551f1663
Zhuojing Wang <zhuojing.wang@ncs.com.sg>
2021-10-23 18:22:45 +0800

    added useraccountinfo model to staffUI models

---
+++ StaffUI/Models/UserAccountInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaffUI.Models
+{
+    public class UserAccountInfo
+    {
+        public string username { get; set; }
+        public Guid accountId { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int mobileNo { get; set; }
+
+
+    }
+}
