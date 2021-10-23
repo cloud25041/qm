36347a9d2d4601e5111ad1333afa588d6657eb96
Zhuojing Wang <zhuojing.wang@ncs.com.sg>
2021-10-23 17:25:35 +0800

    add staffappointmentdetails model in staffUI and staff_API

---
+++ Staff_API/Models/StaffAppointmentDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Staff_API.Models
+{
+    public class StaffAppointmentDetails
+    {
+        public Guid AppointmentId { get; set; }
+        public int AppointmentType { get; set; }
+        public int AppointmentState { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public int AppointmentSlotId { get; set; }
+        public Guid UserAccountId { get; set; }
+        public Guid StaffAccountID { get; set; }
+        public int AgencyId { get; set; }
+        public string ZoomLink { get; set; }
+    }
+}
